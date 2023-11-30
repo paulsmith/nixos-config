5bd8e4a995f83d1c1fb78c01ade7662c5aa9746f
Paul Smith <paulsmith@gmail.com>
2023-11-30 09:59:02 -0600

    venus: upgrade to NixOS 23.11

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "NixOS (and nix-darwin) configuration by paulsmith";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
