c5b9f03f6f3bf9c91da7f5d02bc89fac698a6d96
Paul Smith <paulsmith@gmail.com>
2025-02-19 14:37:07 -0600

    venus: Move Bash extra init code to file sourced by config

---
+++ users/paul/bash/extra.bash
@@ -0,0 +1,12 @@
+export PS1="\[\e[1;34m\]\W\[\e[0m\] \[\e[1;\$(if [ \$? -ne 0 ]; then echo 31; else echo 33; fi)m\]\$\[\e[0m\] "
+
+export MANPATH=$(xcode-select --show-manpaths | tr '\n' ':')
+
+# Ghostty shell integration
+if [ -n "$GHOSTTY_RESOURCES_DIR" ]; then
+    builtin source "${GHOSTTY_RESOURCES_DIR}/shell-integration/bash/ghostty.bash"
+fi
+
+function t {
+    pushd $(mktemp -d -p /tmp -q "${1}.XXXXXX")
+}
--- users/paul/home.nix
+++ users/paul/home.nix
@@ -113,19 +113,9 @@ in
 
   home.sessionVariables.SVDIR = "$HOME/service"; # Runit service directory
 
-  programs.bash.initExtra = ''
-    export PS1="\[\e[1;34m\]\W\[\e[0m\] \[\e[1;\$(if [ \$? -ne 0 ]; then echo 31; else echo 33; fi)m\]\$\[\e[0m\] "
-    export MANPATH=$(xcode-select --show-manpaths | tr '\n' ':')
-
-    # Ghostty shell integration
-    if [ -n "$GHOSTTY_RESOURCES_DIR" ]; then
-        builtin source "''${GHOSTTY_RESOURCES_DIR}/shell-integration/bash/ghostty.bash"
-    fi
-
-    function t {
-        pushd $(mktemp -d -p /tmp -t $1)
-    }
-  '';
+  programs.bash.initExtra = builtins.concatStringsSep "\n" [
+    (builtins.readFile ./bash/extra.bash)
+  ];
 
   home.file."${config.xdg.configHome}/nvim" = {
     source = ../../common/nvim;
