06eaa1748291eb47fe153aeae9cdd3991ba3381b
Paul Smith <paulsmith@pobox.com>
2025-06-04 10:21:05 -0500

    oberon|venus: Turn on automatic gc (weekly, older than 14 days)

--- hosts/oberon/configuration.nix
+++ hosts/oberon/configuration.nix
@@ -122,4 +122,10 @@
       "10MB"
     ];
   };
+
+  nix.gc = {
+    automatic = true;
+    interval = { Weekday = 0; Hour = 0; Minute = 0; };
+    options = "--delete-older-than 14d";
+  };
 }
--- hosts/venus/configuration.nix
+++ hosts/venus/configuration.nix
@@ -87,4 +87,10 @@
       "10MB"
     ];
   };
+
+  nix.gc = {
+    automatic = true;
+    interval = { Weekday = 0; Hour = 0; Minute = 0; };
+    options = "--delete-older-than 14d";
+  };
 }
